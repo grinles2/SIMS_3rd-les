afc19c463ef846ba5bf6212c155be25a0286a5a3
kyrylo <kirillsagusik@gmail.com>
2024-02-03 14:27:47 +0000

    ThirdLesson

--- sims.py
+++ sims.py
@@ -10,25 +10,52 @@ class Human:
         self.money = 100
         self.gladness = 50
         self.satiety = 50
+        self.thirst = 10
 
     def get_home(self):
-        pass
+        self.home = House()
 
     def get_car(self):
-        pass
+        self.car = Auto(brands_of_car)
 
     def get_job(self):
-        pass
+        self.job = Job(job_list)
 
     def eat(self):
+        if self.home.food <= 0:
+            self.shopping("food")
+        else:
+            if self.satiety > 100:
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+
+    def drink(self):
+        if self.home.water <= 0:
+            self.shopping("water")
+            self.thirst -= 20
+            self.home.water -= 5
+
+
+    def shopping(self, manage):
         pass
 
     def work(self):
-        pass
-
-    def shopping(self):
-        pass
-
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 15:
+                self.shopping("Fuel")
+                return
+            else:
+                self.to_repair()
+                return
+        self.money += self.job.salary
+        self.gladness -= self.job.glad
+        self.satiety -= 5
+        self.thirst -= 2
     def chill(self):
         pass
 
@@ -64,6 +91,7 @@ class Auto:
             print("The car broke down")
             return False
 
+
 class House:
     def __init__(self):
         self.mess = 0
@@ -73,7 +101,25 @@ class House:
         self.gas_bill = 0
 
 
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]["salary"]
+        self.glad = job_list[self.job]["glad"]
+
+
+job_list = {"Python dev": {"salary": 50, "glad": 12},
+            "C++ dev": {"salary": 80, "glad": 6},
+            "Php dev": {"salary": 35, "glad": 2}}#
+
 brands_of_car = {"BMW": {"fuel": 100, "strength": 120, "consumption": 14},
                  "Porsche": {"fuel": 170, "strength": 90, "consumption": 99},
                  "Ferrari": {"fuel": 10, "strength": 100, "consumption": 11},
                  "Lamborghini": {"fuel": 60, "strength": 20, "consumption": 1991}}
+
+
+#h = Human("Volodya")
+#h.get_home()
+#print(h.job)
+#h.get_job()
+#print(h.job.job)
